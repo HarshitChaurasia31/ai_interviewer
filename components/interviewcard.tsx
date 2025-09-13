149c68e37b590c5603dab02aad50feb2d6279292
Harshit Chaurasia <harshitchaurasia153@gmail.com>
2025-09-13 21:21:05 +0530

    Auth pages done

---
+++ components/interviewcard.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import dayjs from 'dayjs';
+import Image from 'next/image';
+import { getRandomInterviewCover } from '@/lib/utils';
+const interviewcard = ({ interviewId,userId,role,type,techstack,createdAt }: InterviewCardProps) => {
+  const feedback = null as Feedback | null;
+  const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
+  const formatedDate= dayjs(feedback?.createdAt || createdAt || Date.now()).format('DD MMM, YYYY');
+  return (
+    <div className="card-border w-[300px] max-sm:w-full min-h-96">
+      <div className='card-interview'>
+        <div>
+            <div className='absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600'>
+                <p className='badge-text'>{normalizedType}</p>
+            </div>
+            <Image src={getRandomInterviewCover()} alt="cover-image" width={90} height={90} className='rounded-full object-fitsize-[90px]'/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default interviewcard
